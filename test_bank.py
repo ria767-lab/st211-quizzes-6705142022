90439b0288765c51e6d80c4101f89d346ee602a3
ria767-lab <6705142022@siam.edu>
2026-09-07 11:29:21 +0700

    Add files via upload

---
+++ test_bank.py
@@ -0,0 +1,7 @@
+from bank import BankAccount
+
+def test_deposit_increase_balance():
+    account = BankAccount(balance=100)
+    new_balance = account.deposit(50)
+    assert new_balance == 150
+    
